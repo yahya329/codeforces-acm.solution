1f0c3b7dc743e3e5fd76d55465c92a548e3e3f37
yahya <yahya214465@gmail.com>
2026-01-08 21:42:30 +0200

    Initial Commit set up

---
+++ CodeForce/acm.cpp
@@ -0,0 +1,27 @@
+
+/*
+this acm.fcis <2023> level 1 quistions
+you can find their CodeForce account on this repo :)
+*/
+
+#include<iostream>
+
+using namespace std;
+
+void WaterMelon()
+{
+	int w;
+	cin >> w;
+
+
+	if (w>2 && w % 2 == 0)
+		cout << "YES";
+	else
+		cout << "NO";
+}
+
+int main()
+{
+	//WaterMelon();
+}
+
